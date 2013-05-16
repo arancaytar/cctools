4caab9c35f4aaf1ec865fea3f49f6dc99c2d0693
Aran <aran@ermarian.net>
2013-05-16 17:22:51 +0200

    1.) one-indexed arrays, 2.) typo in relative coordinates, 3.) return to start feature.

--- builder.lua
+++ builder.lua
@@ -9,7 +9,8 @@ function matrixToPositions(matr)
     for j = 1,#(matr[i]),1 do
       if matr[i][j] then
         -- in coordinate systems, the horizontal index precedes.
-        table.insert(positions, {i, j})
+        -- these are relative coordinates; use 0-offset.
+        table.insert(positions, {i-1, j-1})
       end
     end
   end
@@ -24,14 +25,15 @@ function mapPositions(x, z, direction, positions)
   for a = 1,#positions,1 do
     i,j = positions[a][1], positions[a][2]
     if direction == 0 then
-      table.insert(mapped, x+i, z-j)
+      ins = {x+i, z-j}
     elseif direction == 1 then
-      table.insert(mapped, x+j, z+i)
+      ins = {x+j, z+i}
     elseif direction == 2 then
-      table.insert(mapped, x-i, z+i)
+      ins = {x-i, z+j}
     elseif direction == 3 then
-      table.insert(mapped, x-j, z-i)
+      ins = {x-j, z-i}
     end
+    table.insert(mapped, ins)
   end
   return mapped
 end
@@ -59,6 +61,8 @@ end
 -- moves to every position in turn and fills it.
 -- tries to optimize greedily; very good for single-path patterns.
 function buildPositions(y, realPositions)
+  local x, _, z = gps.locate()
+  proxSort(x, z, realPositions)
   n = #realPositions
   while n > 0 do
     x, z = realPositions[n][1], realPositions[n][2]
@@ -67,20 +71,28 @@ function buildPositions(y, realPositions)
     while not placer.checkInventory() do sleep(10) end
     turtle.placeDown()
     table.remove(realPositions)
-    table.sort(x, z, realPositions)
+    proxSort(x, z, realPositions)
     n = n - 1
   end
 end
 
 -- builds a pattern, starting from current position, moving ahead.
 function buildPattern(matrix, count)
-  x, y, z = gps.locate()
-  f = navigation.direction
-  mapped = mapPositions(x, z, f, matrixToPositions(matrix))
+  local sx, sy, sz, sf
+  sx, sy, sz = gps.locate()
+  print("Starting at "..sx.." "..sy.." "..sz)
+  sf = navigation.direction
+  print("Facing "..sf)
+  mapped = mapPositions(sx, sz, sf, matrixToPositions(matrix))
   for i = 0,count-1,1 do
+    print("Level "..i)
     tasks = copyPositions(mapped)
-    buildPositions(y+i, tasks)
+    buildPositions(sy+i, tasks)
   end
+  print("Returning to "..sx.." "..(sy+count).." "..sz)
+  navigation.moveTo(sx, sy+count, sz)
+  print("Turning to "..sf)
+  navigation.turnTo(sf)
 end
 
 function loadPattern(filename)
