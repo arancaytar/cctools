cfded9fed6557a45e431a2b93e14e182cf1bb8c4
Aran <aran@ermarian.net>
2015-06-07 04:17:02 +0200

    %.3f doesn't exist in Lua.

--- power.lua
+++ power.lua
@@ -295,7 +295,7 @@ while true do
             elseif fill >= 0.1 then
                 monitor.setCursorPos(14, 6)
             else monitor.setCursorPos(15, 6) end
-            monitor.write(string.format("%.3f%%", fill * 100))
+            monitor.write(string.format("%f%%", math.floor(fill * 100)))
         elseif getMonitorSize(monitor.getSize()) == "small" then
             --erase old data
             monitor.setCursorPos(10,3)
